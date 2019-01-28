458f6972f61f774900922fc9771bba70b5238e95
Tracy Ngo <tngo1206@gmail.com>
2019-01-28 20:14:15 +0000

    Automatically backed up by Learn

--- index.js
+++ index.js
@@ -3,3 +3,14 @@ function happyHolidays(){
   return "Happy holidays!"
 }
 
+function happyHolidaysTo(name){
+  return `Happy holidays, ${name}!`
+}
+
+function happyHolidayTo(holiday, name){
+  return `Happy ${holiday}, ${name}`
+}
+
+function holidayCountdown(day,holiday){
+  return `It's ${days} days until ${holiday}!`
+}
